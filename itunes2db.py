0c13f313ba358ffe3a1fba99c4906cc87344eb77
KentaGoto <gotokentag@gmail.com>
2023-09-22 11:25:31 +0900

    update

--- itunes2db.py
+++ itunes2db.py
@@ -15,71 +15,73 @@ def get_music_files(folder_path):
                 music_files.append(full_path)
     return music_files
 
-# Get Windows user name.
-windows_username = os.getlogin()
 
-# Target folder root path.
-music_folder_path_root = 'C:\\Users\\' + windows_username + '\\Music\\iTunes\\iTunes Media\\Music'
-
-# List to store music data.
-music_files_list = get_music_files(music_folder_path_root)
-
-# Lists for storing metadata.
-metadata_list = []
-
-# Get metadata values of music data (mp3, m4a).
-for file_path in music_files_list:
-    title, artist, album, track_number = 'Unknown title', 'Unknown artist', 'Unknown album', 'Unknown track'  # Default values
-
-    if file_path.endswith('.mp3'):
-        audio = MP3(file_path, ID3=ID3)
-        title_obj = audio.get('TIT2', 'Unknown title')
-        artist_obj = audio.get('TPE1', 'Unknown artist')
-        album_obj = audio.get('TALB', 'Unknown album')
-        track_number_obj = audio.get('TRCK', 'Unknown track')
-
-        title = title_obj.text[0] if hasattr(title_obj, 'text') else title_obj
-        artist = artist_obj.text[0] if hasattr(artist_obj, 'text') else artist_obj
-        album = album_obj.text[0] if hasattr(album_obj, 'text') else album_obj
-        track_number = track_number_obj.text[0] if hasattr(track_number_obj, 'text') else track_number_obj
-        
-    elif file_path.endswith('.m4a'):
-        audio = MP4(file_path)
-        title = audio.get('\xa9nam', ['Unknown title'])[0]
-        artist = audio.get('\xa9ART', ['Unknown artist'])[0]
-        album = audio.get('\xa9alb', ['Unknown album'])[0]
-        track_number = audio.get('trkn', [(0, 0)])[0][0]
-        
-    metadata_list.append((title, artist, album, track_number))
-
-# Creating and connecting to a SQLite database.
-conn = sqlite3.connect('iTunesMusic.db')
-c = conn.cursor()
-
-# Create a table.
-c.execute('''
-CREATE TABLE IF NOT EXISTS music (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT,
-    artist TEXT,
-    album TEXT,
-    track_number TEXT
-);
-''')
-
-# Insert metadata.
-for metadata in metadata_list:
-    title, artist, album, track_number = metadata
-
-    # Check for duplicates.
-    c.execute("SELECT * FROM music WHERE title = ? AND artist = ? AND album = ? AND track_number = ?",
-              (title, artist, album, track_number))
+if __name__ == '__main__':
+    # Get Windows user name.
+    windows_username = os.getlogin()
     
-    if c.fetchone() is None:
-        c.execute("INSERT INTO music (title, artist, album, track_number) VALUES (?, ?, ?, ?)", 
+    # Target folder root path.
+    music_folder_path_root = 'C:\\Users\\' + windows_username + '\\Music\\iTunes\\iTunes Media\\Music'
+    
+    # List to store music data.
+    music_files_list = get_music_files(music_folder_path_root)
+    
+    # Lists for storing metadata.
+    metadata_list = []
+    
+    # Get metadata values of music data (mp3, m4a).
+    for file_path in music_files_list:
+        title, artist, album, track_number = 'Unknown title', 'Unknown artist', 'Unknown album', 'Unknown track'  # Default values
+    
+        if file_path.endswith('.mp3'):
+            audio = MP3(file_path, ID3=ID3)
+            title_obj = audio.get('TIT2', 'Unknown title')
+            artist_obj = audio.get('TPE1', 'Unknown artist')
+            album_obj = audio.get('TALB', 'Unknown album')
+            track_number_obj = audio.get('TRCK', 'Unknown track')
+    
+            title = title_obj.text[0] if hasattr(title_obj, 'text') else title_obj
+            artist = artist_obj.text[0] if hasattr(artist_obj, 'text') else artist_obj
+            album = album_obj.text[0] if hasattr(album_obj, 'text') else album_obj
+            track_number = track_number_obj.text[0] if hasattr(track_number_obj, 'text') else track_number_obj
+            
+        elif file_path.endswith('.m4a'):
+            audio = MP4(file_path)
+            title = audio.get('\xa9nam', ['Unknown title'])[0]
+            artist = audio.get('\xa9ART', ['Unknown artist'])[0]
+            album = audio.get('\xa9alb', ['Unknown album'])[0]
+            track_number = audio.get('trkn', [(0, 0)])[0][0]
+            
+        metadata_list.append((title, artist, album, track_number))
+    
+    # Creating and connecting to a SQLite database.
+    conn = sqlite3.connect('iTunesMusic.db')
+    c = conn.cursor()
+    
+    # Create a table.
+    c.execute('''
+    CREATE TABLE IF NOT EXISTS music (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        artist TEXT,
+        album TEXT,
+        track_number TEXT
+    );
+    ''')
+    
+    # Insert metadata.
+    for metadata in metadata_list:
+        title, artist, album, track_number = metadata
+    
+        # Check for duplicates.
+        c.execute("SELECT * FROM music WHERE title = ? AND artist = ? AND album = ? AND track_number = ?",
                   (title, artist, album, track_number))
-
-conn.commit()
-conn.close()
-
-print("Done!")
+        
+        if c.fetchone() is None:
+            c.execute("INSERT INTO music (title, artist, album, track_number) VALUES (?, ?, ?, ?)", 
+                      (title, artist, album, track_number))
+    
+    conn.commit()
+    conn.close()
+    
+    print("Done!")
